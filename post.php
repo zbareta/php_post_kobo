a68416bb49884f4debf5b853e8876494ffb5e841
Željko Bareta <zbareta@gmail.com>
2018-12-07 22:38:07 +0100

    Update post.php

--- post.php
+++ post.php
@@ -2,13 +2,13 @@
 if (function_exists('curl_file_create')) {
 	$cFile = curl_file_create(realpath("test.xml"));
 } else {
-	$cFile = '@' . realpath("test.xml");
+	$cFile = '@' . realpath("FILENAME.xml");
 }
 $post = array('xml_submission_file'=> $cFile);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,"https://kobocat.unhcr.org/submission");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_USERPWD, "kobotest:kobotest1123");
+curl_setopt($ch, CURLOPT_USERPWD, "USERNAME:PASSWORD");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result=curl_exec($ch);
